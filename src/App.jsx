859c112c99ce1a60986502fa3367e805fc675464
Senesh Nagodavithana <seneshnagodavithana@gmail.com>
2024-11-05 21:05:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import DigitalClock from "./DigitalClock/DigitalClock";
+
+function App() {
+  return (
+    <>
+      <DigitalClock></DigitalClock>
+    </>
+  );
+}
+
+export default App;
